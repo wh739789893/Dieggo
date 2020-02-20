64642baae2da4c2c629441dfdc1dabaf10405ed8
敖图图 <wh739789893@qq.com>
2020-02-20 17:23:34 +0800

    样式适配 框架适配

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import 'amfe-flexible'
 
 Vue.config.productionTip = false
+
+Vue.use(Vant)
 
 new Vue({
   router,
