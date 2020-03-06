21ae2be1e17ebac04ed7764cbcd45b7be3972c98
敖图图 <wh739789893@qq.com>
2020-03-06 11:34:24 +0800

    Vuex解决数据共享问题

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as auth from '@/utils/auth'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: auth.getUser()
   },
   mutations: {
+    updateUser (state, payload) {
+      state.user = payload.user // 更新数据
+      auth.setUser(payload.user) // 数据同步设置到本地存储
+    },
+
+    // 清空user
+    clearUser (state) {
+      state.user = {}
+      auth.delUser() // 缓存数据清空
+    }
   },
   actions: {
   },
