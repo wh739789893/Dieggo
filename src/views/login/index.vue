f752ccb44ae2a0d5596da9d1c211c41ec6c0014d
敖图图 <wh739789893@qq.com>
2020-03-06 15:11:26 +0800

    登陆页面修正

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,17 +1,17 @@
 <template>
   <div class='container'>
     <van-nav-bar title="登录" left-arrow @click-left="$router.back()"></van-nav-bar>
-    <!-- 手机号 验证码 登录按钮 -->
+    <!-- 手机号  验证码  登录按钮 -->
     <van-cell-group>
-      <van-filed label="手机号" placeholder="请输入手机号"></van-filed>
-      <van-filed label="验证码" placeholder="请输入验证码">
-          <van-button slot="button" size="small" type="primary">发送验证码</van-button>
-      </van-filed>
+      <van-field  label="手机号" placeholder="请输入手机号"></van-field>
+      <van-field  label="验证码" placeholder="请输入验证码">
+        <!-- slot指定给哪个坑填内容 -->
+        <van-button  slot="button" size="small" type="primary">发送验证码</van-button>
+      </van-field>
     </van-cell-group>
-
-        <div class="btn-box">
-            <van-button type="info" size="small" round block>登录</van-button>
-        </div>
+    <div class='btn-box'>
+      <van-button type="info" size="small" round block>登录</van-button>
+    </div>
   </div>
 </template>
 
@@ -22,7 +22,7 @@ export default {
 </script>
 
 <style scoped>
-  .btn-box{
-    padding: 20px;
-  }
+   .btn-box {
+     padding: 20px;
+   }
 </style>
