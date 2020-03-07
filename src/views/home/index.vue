9c304a0ce51bf92d1632464020adbfb0e4b24014
敖图图 <wh739789893@qq.com>
2020-03-08 07:54:06 +0800

    文章列表初步布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+     <van-tabs v-model="activeIndex" swipeable>
+       <van-tab :title="'标签' + item" v-for="item in 10" :key="item">
+          <!-- 阅读记忆 -->
+          <div class="scroll-wrapper">
+              <van-cell-group>
+                 <van-cell v-for="obj in 20" :key="obj" :title="item"></van-cell>
+              </van-cell-group>
+          </div>
+       </van-tab>
+     </van-tabs>
+     <span class="bar_btn">
+        <van-icon name="wap-nav"></van-icon>
+     </span>
+  </div>
 </template>
 
 <script>
@@ -9,4 +23,59 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.van-tabs {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  /deep/ .van-tabs__wrap {
+    height: 36px;
+    padding-right: 36px;
+    .van-tab {
+      line-height: 36px;
+    }
+    .van-tabs__line {
+      background-color: #3296fa;
+      height: 2px;
+    }
+  }
+  /deep/ .van-tabs__content{
+    flex: 1;
+    overflow: hidden;
+  }
+  /deep/ .van-tab__pane{
+    height: 100%;
+    .scroll-wrapper{
+      height: 100%;
+      overflow-y: auto;
+    }
+  }
+}
+.bar_btn {
+  width: 36px;
+  height: 35px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  &::before {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 999;
+    box-shadow: 0 0 10px #999;
+    transform: scale(1, 0.6);
+  }
+  .van-icon-wap-nav {
+    width: 100%;
+    height: 100%;
+    background: #fff;
+    text-align: center;
+    line-height: 35px;
+    position: relative;
+    z-index: 1000;
+    &::before {
+      font-size: 20px;
+    }
+  }
+}
 </style>
