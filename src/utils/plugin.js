44c2b430b77184abc470dc152896c25100b81bc6
敖图图 <wh739789893@qq.com>
2020-03-08 11:26:04 +0800

    注册一个全局相对时间过滤器

--- src/utils/plugin.js
+++ src/utils/plugin.js
@@ -1,8 +1,13 @@
 
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/zh-cn' // 引入中文的语言包
+dayjs.extend(relativeTime)
 export default {
   install (Vue) {
     Vue.prototype.$gnotify = (params) => Vue.prototype.$notify({ duration: 800, ...params }) // 封装一个提示消息
     Vue.prototype.$sleep = sleep // 封装延迟处理函数
+    Vue.filter('relTime', relTime) // 注册全局相对时间过滤器
   }
 }
 
@@ -10,4 +15,8 @@ function sleep (time = 500) {
   return new Promise(function (resolve, reject) {
     setTimeout(() => resolve(), time)
   })
+}
+
+function relTime (value) {
+  return dayjs().locale('zh-cn').from(value)
 }
