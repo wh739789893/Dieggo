51153169e07753d3018c919f898b7c5a82270d6c
敖图图 <wh739789893@qq.com>
2020-03-08 08:43:52 +0800

    文章列表布局

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -2,7 +2,28 @@
   <div class="scroll-wrapper">
     <van-pull-refresh v-model="downLoading" @refresh="onRefresh" :success-text="refreshSuccessText">
       <van-list v-model="upLoading" :finished="finished" @load="onLoad" finished-text="我也是有底线的!.....">
-         <van-cell v-for="article in articles" :key="article" :title="article"></van-cell>
+         <van-cell v-for="article in articles" :key="article" :title="article">
+             <!-- 开启布局 -->
+                <div class="article_item">
+                  <h3 class="van-ellipsis">PullRefresh下拉刷新PullRefresh下拉刷新下拉刷新下拉刷新</h3>
+                  <!-- 三图模式 -->
+                  <div class="img_box">
+                    <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg"/>
+                    <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg"/>
+                    <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg"/>
+                 </div>
+                 <!-- 单图模式 -->
+                   <div class="img_box">
+                      <van-image class="w100" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg"/>
+                  </div>
+                <div class="info_box">
+                  <span>你像一阵风</span>
+                  <span>8评论</span>
+                  <span>10分钟前</span>
+                  <span class="close"><van-icon name="cross"></van-icon></span>
+                </div>
+              </div>
+         </van-cell>
       </van-list>
     </van-pull-refresh>
   </div>
@@ -49,4 +70,44 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.article_item{
+  h3{
+    font-weight: normal;
+    line-height: 2;
+  }
+  .img_box{
+    display: flex;
+    justify-content: space-between;
+    .w33{
+      width: 33%;
+      height: 90px;
+    }
+    .w100{
+      width: 100%;
+      height: 180px;
+    }
+  }
+  .info_box{
+    color: #999;
+    line-height: 2;
+    position: relative;
+    font-size: 12px;
+    span{
+      padding-right: 10px;
+      &.close{
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        line-height: 15px;
+        height: 12px;
+        width: 16px;
+        text-align: center;
+        padding-right: 0;
+        font-size: 8px;
+        position: absolute;
+        right: 0;
+        top: 7px;
+      }
+    }
+  }
+}
 </style>
