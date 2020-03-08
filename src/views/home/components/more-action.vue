450dbe5576348b6e42dd7b9666884809f9d9d259
敖图图 <wh739789893@qq.com>
2020-03-08 15:11:30 +0800

    子组件布局  封装举报文章接口

--- src/views/home/components/more-action.vue
+++ src/views/home/components/more-action.vue
@@ -7,23 +7,19 @@
     </van-cell-group>
     <van-cell-group v-else>
       <van-cell icon="arrow-left" @click="isReport=false">返回</van-cell>
-      <van-cell>侵权</van-cell>
-      <van-cell>色情</van-cell>
-      <van-cell>暴力</van-cell>
-      <van-cell>低俗</van-cell>
-      <van-cell>不适</van-cell>
-      <van-cell>错误</van-cell>
-      <van-cell>其他</van-cell>
+      <van-cell icon="info-o"  @click="$emit('report',item.value)"    v-for="item in reports" :key="item.value">{{item.label}}</van-cell>
     </van-cell-group>
   </div>
 </template>
 
 <script>
+import { reports } from '@/api/constants'
 import eventBus from '@/utils/eventBus'
 export default {
   data () {
     return {
-      isReport: false
+      isReport: false, // 控制第一个和第二个单元格显示
+      reports
     }
   },
 
