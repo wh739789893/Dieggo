058f7387937b5f25d0cf86fcb92218d451ec1d19
敖图图 <wh739789893@qq.com>
2020-03-08 11:13:21 +0800

    封装延迟处理函数

--- src/utils/plugin.js
+++ src/utils/plugin.js
@@ -2,5 +2,12 @@
 export default {
   install (Vue) {
     Vue.prototype.$gnotify = (params) => Vue.prototype.$notify({ duration: 800, ...params }) // 封装一个提示消息
+    Vue.prototype.$sleep = sleep // 封装延迟处理函数
   }
+}
+
+function sleep (time = 500) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => resolve(), time)
+  })
 }
