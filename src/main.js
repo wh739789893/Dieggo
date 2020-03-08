f2748ab05db7fc445e894f2a84766a9de11be400
敖图图 <wh739789893@qq.com>
2020-03-08 14:18:35 +0800

    实现图片懒加载

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vant from 'vant'
+import Vant, { Lazyload } from 'vant'
 import plugin from '@/utils/plugin'
 import '@/styles/index.less' // 引入全局的自定义样式  因为要覆盖vant的样式
 import 'vant/lib/index.css'
@@ -12,6 +12,7 @@ Vue.config.productionTip = false
 
 Vue.use(Vant)
 Vue.use(plugin)
+Vue.use(Lazyload)
 
 new Vue({
   router,
