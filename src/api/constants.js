330cd929f2664da58c2892423b091410d3c8a0dd
敖图图 <wh739789893@qq.com>
2020-03-08 15:05:51 +0800

    封装举报文章接口

---
+++ src/api/constants.js
@@ -0,0 +1,30 @@
+
+// 举报类型： 0-其他问题，1-标题夸张，2-低俗色情，3-错别字多，4-旧闻重复，5-广告软文，6-内容不实，7-涉嫌违法犯罪，8-侵权'
+export const reports = [{
+  value: 0,
+  label: '其他问题'
+}, {
+  value: 1,
+  label: '标题夸张'
+}, {
+  value: 2,
+  label: '低俗色情'
+}, {
+  value: 3,
+  label: '错别字多'
+}, {
+  value: 4,
+  label: '旧闻重复'
+}, {
+  value: 5,
+  label: '广告软文'
+}, {
+  value: 6,
+  label: '内容不实'
+}, {
+  value: 7,
+  label: '涉嫌违法犯罪'
+}, {
+  value: 8,
+  label: '侵权'
+}]
