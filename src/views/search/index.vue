0c92b0d285cebd20ab22887a0b6a858ca122c1c3
敖图图 <wh739789893@qq.com>
2020-03-09 15:15:37 +0800

    搜索页面功能完成 搜索结果页面静态布局

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -3,29 +3,36 @@
     <!-- 搜索组件一级路由   返回上一个页面-->
     <van-nav-bar left-arrow title="搜索中心" @click-left="$router.back()"></van-nav-bar>
     <!-- 导航 -->
-    <van-search   @click="onSearch"    v-model.trim="q"    placeholder="请输入搜索关键词" shape="round" />
-    <!--
-      什么时候出现联想搜索, ? 什么时候出现 历史记录 ?
-
-      应该是有输入内容时  =>联想
-
-      没内容时  => 历史记录
-
-     -->
-    <van-cell-group class="suggest-box"  v-if="q">
-      <van-cell icon="search">
-        <span>j</span>ava
+    <!-- 搜索框 -->
+    <!-- trim自动去空格的修饰符 -->
+    <van-search @search="onSearch" v-model.trim="q" placeholder="请输入搜索关键词" shape="round" />
+    <!-- 联想搜索 -->
+    <van-cell-group class="suggest-box" v-if="q">
+      <van-cell @click="toSearchResult(item)" icon="search" v-for="item in suggestList" :key="item">
+        {{ item }}
       </van-cell>
     </van-cell-group>
+    <!-- 历史记录 -->
+    <!-- 只有当有历史记录的时候 才显示整个历史的记录的盒子 -->
     <div class="history-box" v-else-if="historyList.length">
       <div class="head">
         <span>历史记录</span>
+        <!-- 清空所有历史 -->
         <van-icon name="delete" @click="clear"></van-icon>
       </div>
       <van-cell-group>
-        <van-cell   @click="toSearchResult(text)"   v-for="(item,index) in historyList" :key="index">
-          <a class="word_btn">{{item}}</a>
-          <van-icon   @click="delHistory(index)"     class="close_btn" slot="right-icon" name="cross" />
+        <!-- 循环生成历史记录  item是惟一的 搜索历史记录不应该重复 -->
+        <van-cell @click="toResult(item)" v-for="(item,index) in historyList" :key="item">
+          <!-- item就是搜索的关键字 -->
+          <a class="word_btn">{{ item }}</a>
+          <!-- 给删除按钮注册删除事件 -->
+          <!-- 事件修饰符 事件名.stop 表示 阻止了事件冒泡 -->
+          <van-icon
+            @click.stop="delHistory(index)"
+            class="close_btn"
+            slot="right-icon"
+            name="cross"
+          />
         </van-cell>
       </van-cell-group>
     </div>
@@ -45,10 +52,11 @@ export default {
     }
   },
 
+  created () {
+    this.historyList = JSON.parse(localStorage.getItem(key) || '[]') // 读取本地化内容
+  },
+
   methods: {
-    created () {
-      this.historyList = JSON.parse(localStorage.getItem(key) || '[]') // 读取本地化内容
-    },
 
     // 删除对应历史记录
     delHistory (index) {
@@ -77,6 +85,16 @@ export default {
 
       // 携带参数去搜索结果页面
       this.$router.push({ path: '/search/result', query: { q: this.q } })
+    },
+
+    // 点击联想搜索关键词 去跳转 => 先把点击的关键词放入历史记录 表示我搜索过
+    toSearchResult (text) {
+      // 放入历史记录
+      const obj = new Set(this.historyList) // 生成一个set变量  set对象自动去重
+      obj.add(text)
+      this.historyList = Array.from(obj) // 将set转回数组
+      localStorage.setItem(key, JSON.stringify(this.historyList)) // 重新写入缓存
+      this.$router.push({ path: '/search/result', query: { q: text } }) // 直接跳转到搜索结果界面
     }
   },
 
