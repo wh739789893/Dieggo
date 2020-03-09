7793cba565d55f3dd4b6c221d7538825e5f0df6c
敖图图 <wh739789893@qq.com>
2020-03-09 16:59:44 +0800

    渲染结构

--- src/views/article/components/comment.vue
+++ src/views/article/components/comment.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="comment">
     <van-list  @click="onLoad"  v-model="loading" :finished="finished" finished-text="没有更多了">
-      <div class="item van-hairline--bottom van-hairline--top" v-for="index in 5" :key="index">
+      <div class="item van-hairline--bottom van-hairline--top" v-for="comment in comments" :key="comment.com_id.toString()">
         <van-image
           round
           width="1rem"
           height="1rem"
           fit="fill"
-          src="https://img.yzcdn.cn/vant/cat.jpeg"
+          src="comment.aut_photo"
         />
         <div class="info">
           <p>
-            <span class="name">一阵清风</span>
+            <span class="name">{{comment.aut_name}}</span>
             <span style="float:right">
               <span class="van-icon van-icon-good-job-o zan"></span>
-              <span class="count">10</span>
+              <span class="count">{{comment.like_count}}</span>
             </span>
           </p>
-          <p>评论的内容，。。。。</p>
+          <p>{{comment.content}}</p>
           <p>
-            <span class="time">两天内</span>&nbsp;
-            <van-tag plain @click="showReply=true">4 回复</van-tag>
+            <span class="time">{{ comment.pubdate | relTime }}</span>&nbsp;
+            <van-tag plain @click="showReply=true">{{ comment.reply_count }} 回复</van-tag>
           </p>
         </div>
       </div>
@@ -33,7 +33,6 @@
     </div>
   </div>
 
-  <!-- 输入框 -->
 </template>
 
 <script>
