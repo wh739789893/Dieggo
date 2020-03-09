619ba3ea82700d73925db4be291c02593e4bb2dc
敖图图 <wh739789893@qq.com>
2020-03-09 14:52:17 +0800

    实时查询对应api

--- src/api/article.js
+++ src/api/article.js
@@ -31,3 +31,13 @@ export function reportArticle (data) {
     method: 'post'
   })
 }
+
+/***
+ *获取文章搜索的建议
+ * **/
+export function suggestion (params) {
+  return request({
+    url: '/suggestion',
+    params
+  })
+}
