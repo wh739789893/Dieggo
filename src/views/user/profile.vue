1911ba5848ea12a9f82316350eea5708fa35b64f
敖图图 <wh739789893@qq.com>
2020-03-10 17:29:56 +0800

    四个弹出层组件配置

--- src/views/user/profile.vue
+++ src/views/user/profile.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+      <!-- 导航 -->
     <van-nav-bar left-arrow @click-left="$router.back()" title="编辑资料" right-text="保存" ></van-nav-bar>
     <van-cell-group>
       <van-cell is-link title="头像"  center>
@@ -16,12 +17,49 @@
       <van-cell is-link title="性别" value='男'/>
       <van-cell is-link title="生日" value="2019-08-08" />
     </van-cell-group>
+
+    <!-- 弹层组件开始 -->
+    <!-- 头像弹层 -->
+    <van-popup v-model="showPhoto" style="width:80%">
+        <!-- 内容 -->
+        <van-cell is-link  title="本地相册选择图片"></van-cell>
+        <van-cell is-link  title="拍照"></van-cell>
+    </van-popup>
+
+    <!-- 昵称弹层 -->
+    <van-popup v-model="showName" style="width:80%">
+        <!-- 编辑用户昵称  双向绑定用户昵称 -->
+        <van-filed v-model="user.name" type="textarea" rows="4"></van-filed>
+    </van-popup>
+
+    <!-- 性别弹层 -->
+    <van-action-sheet :actions="actions" v-model="showGender" cancel-text="取消"></van-action-sheet>
+
+    <!-- 生日弹层 -->
+    <van-popup v-model="showBirthDay" position="bottom">
+       <!-- type表示 当前的日期类型 年月日 -->
+        <van-datetime-picker
+          v-model="currentDate"
+          type="date"
+          :min-date="minDate"
+          :max-date="maxDate"
+        >
+        </van-datetime-picker>
+    </van-popup>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'profile'
+  name: 'profile',
+  data () {
+    return {
+      showBirthDay: false, // 是否显示日期弹层
+      showPhoto: false, // 是否显示选择头像弹层
+      showName: false, // 是否显示编辑昵称的弹层
+      showGender: false // 是否显示性别选择的弹层
+    }
+  }
 }
 </script>
 
