db13aa26e278f877cc7bdcf4c88e91455ce86c7a
敖图图 <wh739789893@qq.com>
2020-03-10 17:51:09 +0800

    引入时间处理函数  几个弹层处理

--- src/views/user/profile.vue
+++ src/views/user/profile.vue
@@ -29,13 +29,13 @@
     <!-- 昵称弹层  关闭点击弹层 关闭功能-->
     <van-popup   :close-on-click-overlay="false"     v-model="showName" style="width:80%">
         <!-- 编辑用户昵称  双向绑定用户昵称 -->
-        <van-filed v-model.trim="user.name" type="textarea" rows="4"></van-filed>
+        <van-filed    :error-message="nameMsg"   v-model.trim="user.name" type="textarea" rows="4"></van-filed>
         <!-- 关闭按钮组件 -->
         <van-button type="info" size="large" block @click="btnName">确定</van-button>
     </van-popup>
 
     <!-- 性别弹层 -->
-    <van-action-sheet :actions="actions" v-model="showGender" cancel-text="取消"></van-action-sheet>
+    <van-action-sheet  @select="selectItem"     :actions="actions" v-model="showGender" cancel-text="取消"></van-action-sheet>
 
     <!-- 生日弹层 -->
     <van-popup v-model="showBirthDay" position="bottom">
@@ -45,6 +45,8 @@
           type="date"
           :min-date="minDate"
           :max-date="maxDate"
+          @cancel="showBirthDay=false"
+          @confirm="confirmDate"
         >
         </van-datetime-picker>
     </van-popup>
@@ -52,6 +54,8 @@
 </template>
 
 <script>
+import dayjs from 'dayjs'
+// import { mapMutations } from 'vuex'
 export default {
   name: 'profile',
   data () {
@@ -65,6 +69,36 @@ export default {
       showPhoto: false, // 是否显示选择头像弹层
       showName: false, // 是否显示编辑昵称的弹层
       showGender: false // 是否显示性别选择的弹层
+    }
+  },
+
+  methods: {
+    // 昵称长度判断
+    btnName () {
+      if (this.user.name.length < 1 || this.user.name.length > 7) {
+        this.nameMsg = '您的用户昵称不符合1-7的长度要求'
+        return false
+      }
+      this.nameMsg = '' // 提示消息清空
+      this.showName = false // 关闭弹层
+    },
+
+    //  捕获点击上拉菜单选项事件
+    selectItem (item) {
+      this.user.gender = item.name === '男' ? 0 : 1 // 根据判断得到当前性别
+      this.showGender = false // 关闭当前弹层
+    },
+
+    // 设置生日弹层 触发方法
+    confirmDate (date) {
+      this.user.birthday = dayjs(date).format('YYYY-MM-DD') // 转化后结果赋值给user生日
+      this.showBirthDay = false
+    },
+
+    // 点击生日 触发
+    showDate () {
+      this.currentDate = new Date(this.user.birthday) // 当前用户生日 赋值当前绑定时间数据
+      this.showBirthDay = true // 显示生日弹层
     }
   }
 }
