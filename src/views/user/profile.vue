cbd66db6ae053a8853f3b97c2507751a999b91ac
敖图图 <wh739789893@qq.com>
2020-03-10 17:13:18 +0800

    编辑资料界面初始布局完成

--- src/views/user/profile.vue
+++ src/views/user/profile.vue
@@ -1,6 +1,21 @@
 <template>
-  <div class='container'>
-    <van-nav-bar title="编辑资料" left-arrow  right-text="保存" @click-left="$router.back()"></van-nav-bar>
+    <div class="container">
+    <van-nav-bar left-arrow @click-left="$router.back()" title="编辑资料" right-text="保存" ></van-nav-bar>
+    <van-cell-group>
+      <van-cell is-link title="头像"  center>
+        <van-image
+          slot="default"
+          width="1.5rem"
+          height="1.5rem"
+          fit="cover"
+          round
+          src="https://img.yzcdn.cn/vant/cat.jpeg"
+        />
+      </van-cell>
+      <van-cell is-link title="名称" value="用户名称" />
+      <van-cell is-link title="性别" value='男'/>
+      <van-cell is-link title="生日" value="2019-08-08" />
+    </van-cell-group>
   </div>
 </template>
 
